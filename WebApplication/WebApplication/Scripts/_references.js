7cd1d87ff9713630c00a7752c22633b072dd1e51
Lab-mvc <serhiipetulkouber@gmail.com>
2019-02-10 22:54:00 +0100

    SP

---
+++ WebApplication/WebApplication/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="jquery-3.3.1.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="respond.js" />
+/// <reference path="chat.js" />
+/// <reference path="jquery.unobtrusive-ajax.js" />
