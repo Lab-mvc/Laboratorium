4fb4c44a8512c03bfb677c4e7396773c6cd935dc
DamianPaw <46650102+DamianPaw@users.noreply.github.com>
2019-02-10 21:18:22 +0100

    Update chat.js
    DP

--- WebApplication/WebApplication/Scripts/chat.js
+++ WebApplication/WebApplication/Scripts/chat.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-  
     // login
     $("#btnLogin").click(function () {
         var nickName = $("#txtUserName").val();
         if (nickName) {
            var href = "/Home?user=" + encodeURIComponent(nickName);      
+           href = href + "&logOn=true";
+           $("#LoginButton").attr("href", href).click();
+          //pole
