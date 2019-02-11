40a471d5e070ac97d087353f76ea0dd5b1ad5863
DamianPaw <46650102+DamianPaw@users.noreply.github.com>
2019-02-11 21:50:31 +0100

    Update chat.js
    DP

--- WebApplication/WebApplication/Scripts/chat.js
+++ WebApplication/WebApplication/Scripts/chat.js
@@ -1,79 +1,78 @@
   $(document).ready(function () {
+
     // login
     $("#btnLogin").click(function () {
         var nickName = $("#txtUserName").val();
         if (nickName) {
-           var href = "/Home?user=" + encodeURIComponent(nickName);      
-           href = href + "&logOn=true";
-           $("#LoginButton").attr("href", href).click();
-          //pole
-            $("Username").text(nickName);
+            var href = "/Home?user=" + encodeURIComponent(nickName);
+            href = href + "&logOn=true";
+            $("#LoginButton").attr("href", href).click();
+            //pole
+            $("#Username").text(nickName);
         }
     });
- });
- //przejście dalej
- function LoginOnSuccess(result) {
-    
+});
+
+//przejscie dalej 
+function LoginOnSuccess(result) {
+
     Scroll();
     ShowLastRefresh();
-    
+
     //aktualizacja co 5 sek
     setTimeout("Refresh();", 5000);
-    
-    //wysyłanie wiadomości za pomoca enter
-    $( '#txtMessage ').keydown(function) (e) {
+
+    //wysylanie wiadomosci za pomoca enter
+    $('#txtMessage').keydown(function (e) {
         if (e.keyCode == 13) {
             e.preventDefault();
             $("#btnMessage").click();
         }
     });
-    
+
     $("#btnMessage").click(function () {
         var text = $("#txtMessage").val();
         if (text) {
-            //za pomoca Index przesyłamy parametr "chatMessage" 
+            //za pomoca Index przesylamy parametr "chatMessage"
             var href = "/Home?user=" + encodeURIComponent($("#Username").text());
             href = href + "&chatMessage=" + encodeURIComponent(text);
             $("#ActionLink").attr("href", href).click();
             $("#txtMessage").empty();
         }
     });
-    
-        //btn wyjscia
+
+    //btn wyjscia
     $("#btnLogOff").click(function () {
-        //za pomoca Index przesyłamy parametr "logOff"
+        //za pomoca Index przesylamy parametr "logOff"
         var href = "/Home?user=" + encodeURIComponent($("#Username").text());
         href = href + "&logOff=true";
         $("#ActionLink").attr("href", href).click();
         document.location.href = "Home";
     });
-    }
-    //komunikat o zly login 
-    function LoginOnFailure(result) {
-      $("#Username").val("");
-      $("#Error").text(result.responseText);
-      setTimeout("$('#Error').empty();", 2000);
-    }
-      //komunikat zły login 
-      function LoginOnFailure(result) {
-       $("#Username").val("");
-      $("#Error").text(result.responseText);
-      setTimeout("$('#Error').empty();", 2000);
-    } 
+}
+
+//komunikat o zly login 
+function LoginOnFailure(result) {
+    $("#Username").val("");
+    $("#Error").text(result.responseText);
+    setTimeout("$('#Error').empty();", 2000);
+}
+
 // aktualizacja co 5 sec
 function Refresh() {
     var href = "/Home?user=" + encodeURIComponent($("#Username").text());
     $("#ActionLink").attr("href", href).click();
     setTimeout("Refresh();", 5000);
 }
+
 //komunikat
 function ChatOnFailure(result) {
     $("#Error").text(result.responseText);
     setTimeout("$('#Error').empty();", 2000);
 }
+
 function ChatOnSuccess(result) {
     Scroll();
-    Show();
     ShowLastRefresh();
 }
 
@@ -83,7 +82,12 @@ function Scroll() {
     win.scrollTop(height);
 }
 
-  
+//ostatna aktualizacja
+function ShowLastRefresh() {
+    var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    $("#LastRefresh").text("Ostatna aktualizacja : " + time);
+}
     
     
     
