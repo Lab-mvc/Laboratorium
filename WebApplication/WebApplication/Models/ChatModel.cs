29044dd84c7e3926ab771903ba0f9c42fff9727a
HubertKopysc <47505273+HubertKopysc@users.noreply.github.com>
2019-02-11 20:40:18 +0100

    Update 1

--- WebApplication/WebApplication/Models/ChatModel.cs
+++ WebApplication/WebApplication/Models/ChatModel.cs
@@ -4,7 +4,24 @@ using System.Linq;
 using System.Web;
 
 namespace WebApplication.Models{
-  public class ChatModel{}
-  public class ChatUser{}
-  public class ChatMessage{}
+  public class ChatModel{
+      // lista uzytkownnkow
+        public List<ChatUser> Users;
+
+        // Wiadomosci
+        public List<ChatMessage> Messages;
+  }
+  public class ChatUser{
+      public string Name;
+        public DateTime LoginTime;
+        public DateTime LastPing;
+  }
+  public class ChatMessage{
+      // autor
+        public ChatUser User;
+        // czas
+        public DateTime Date = DateTime.Now;
+        // tekst
+        public string Text = "";
+  }
 }
